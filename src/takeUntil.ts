f6b43dc2153bd0742ceb0a78fe91be8204c5413d
Chris Sperry <christophermartinsperry@gmail.com>
2019-04-20 04:30:47 +0100

    fix: reimplement takeUntil

--- src/takeUntil.ts
+++ src/takeUntil.ts
@@ -1,16 +1,39 @@
-import { deferP0 } from 'ts-functional-pipe'
+import { deferP0, pp } from 'ts-functional-pipe'
 import { _merge } from './merge'
 import { _take } from './take'
+import { createPromiseResolver } from 'ts-async-iterable-queue'
+import { _map, map } from './map'
 
 export async function* _takeUntil<T, TOther>(
   src: AsyncIterable<T>,
   other: AsyncIterable<TOther>
 ): AsyncIterable<T> {
-  for await (const [srcItem, otherItem] of _merge(src, _take(other, 1))) {
-    if (otherItem !== undefined) {
-      break
+  const pr = createPromiseResolver()
+
+    // tslint:disable-next-line: no-floating-promises
+  ;(async () => {
+    for await (const _ of _take(other, 1)) {
+      pr.resolve()
     }
-    yield srcItem!
+  })()
+
+  const it = src[Symbol.asyncIterator]()
+  try {
+    for (;;) {
+      const r = await Promise.race([pr.promise, it.next()])
+      if (typeof r === 'undefined') {
+        break
+      }
+      /* istanbul ignore if */
+      if (r.done) {
+        break
+      }
+      yield r.value
+    }
+    it.return && (await it.return())
+  } catch (e) {
+    /* istanbul ignore next */
+    it.throw && it.throw(e)
   }
 }
 
